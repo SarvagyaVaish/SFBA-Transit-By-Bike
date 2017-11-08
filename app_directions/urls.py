7cdc8519b34dc201a2d232b1e1bf908c389d0ad4
Survy <sarvagya.vaish7@gmail.com>
2017-11-07 17:07:17 -0800

    Separate API and website urls

--- app_directions/urls.py
+++ app_directions/urls.py
@@ -3,6 +3,13 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	# Example: /?from_loc=37.4258,-122.1001&to_loc=37.7927,-122.3970&start_time=18:00:00
 	url(r'^$', views.index, name='index'),
+
+	# API
+	# Example: /?from_loc=37.4258,-122.1001&to_loc=37.7927,-122.3970&start_time=18:00:00
+	url(r'^api$', views.api, name='api'),
+
+	# GUI
+	url(r'^search$', views.search, name='search'),
+	url(r'^result$', views.result, name='result'),
 ]
--- app_directions/views.py
+++ app_directions/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse, JsonResponse
 from scripts.main import find_routes
 
 
+# Helper function
 def get_lat_lon_tuple(coordinate_str):
 	coordinates = coordinate_str.split(",")
 	lat = float(coordinates[0])
@@ -15,6 +16,10 @@ def get_lat_lon_tuple(coordinate_str):
 
 
 def index(request):
+	return HttpResponse("Hi Kurt!")
+
+
+def api(request):
 	from_coordinate = request.GET.get('from_loc', '')
 	to_coordinate = request.GET.get('to_loc', '')
 	start_time = request.GET.get('start_time', '')
@@ -25,3 +30,11 @@ def index(request):
 			start_time
 		)
 	return JsonResponse(solutions)
+
+
+def search(request):
+	return HttpResponse("search")
+
+
+def result(request):
+	return HttpResponse("result")
